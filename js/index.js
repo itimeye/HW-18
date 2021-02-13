7132cea819ef831a4223379bda2a507f427fb22d
Eyenekbi <itima.eye@gmail.com>
2021-02-13 13:57:19 -0600

    add google analytics

---
+++ js/index.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $(".recipeRoulette").on("mouseover",function(){
+        $('.nameR').show();
+    }).on("mouseleave", function() {
+        $(".nameR").hide();
+    });
+
+
+});
+
+$(document).ready(function() {
+    $(".workersDefense").on("mouseover",function(){
+        $('.nameW').show();
+    }).on("mouseleave", function() {
+        $(".nameW").hide();
+    });
+
+
+});
+
+$(document).ready(function() {
+    $(".NSA").on("mouseover",function(){
+        $('.nameN').show();
+    }).on("mouseleave", function() {
+        $(".nameN").hide();
+    });
+
+
+});
